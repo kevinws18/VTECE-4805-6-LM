211f609a23a6cba53f341b1fe0915a951283ac0b
santoshk315 <santoshk@vt.edu>
2021-10-23 16:57:19 -0400

    fix demod bugs

--- PythonImplementation/SOQPSK-TG_demodulation.py
+++ PythonImplementation/SOQPSK-TG_demodulation.py
@@ -7,10 +7,9 @@ import matplotlib.pyplot as plt
 def SOQPSK_TG_Demod(preamble, signal, SPS):
     samples_per_bit = SPS/2
 
-    phase = (np.unwrap(np.angle(signal)) - 0.785416) / np.pi;
+    phase = (np.unwrap(np.angle(signal)) - 0.785416) / np.pi
 
     filtered = np.zeros(len(phase))
-
     for x in range(1, len(phase)-1, 1):
         filtered[x] = phase[x+1] - phase[x]
 
@@ -21,21 +20,19 @@ def SOQPSK_TG_Demod(preamble, signal, SPS):
             padded[i] = 1
         elif(filtered[i] < -1 * filter_offset):
             padded[i] = -1
-        else:
-            padded[i] = 0
-    alpha = np.zeros(len(padded)/samples_per_bit)
-    for x in range(1, len(alpha) - 1, 1):
-        alpha[x] = padded((i-1) * samples_per_bit)
 
-    starter = preamble[-2:]
+    alpha = np.zeros(int(len(padded)/samples_per_bit))
+    for x in range(1, len(alpha) - 1, 1):
+        alpha[x-1] = padded[(int((x-1) * samples_per_bit))]
+
+    starter = [preamble[len(preamble) - 2], preamble[len(preamble) - 1]]
     bits = np.zeros(len(alpha))
     bits[0] = starter[0]
     bits[1] = starter[1]
     for x in range(2, len(alpha) - 1, 1):
-        if(((-1 ** (x+1)) * (2 * bits[x - 1] - 1) * (1- bits[x-2])) == alpha[x]):
+        if((([1 if x%2 == 0 else -1][0]) * (2 * bits[x - 1] - 1) * (1- bits[x-2])) == alpha[x]):
             bits[x] = 1
-        else:
-            bits[x] = 0
 
-    bits = bits[0:-4]
+    bits = bits[4:len(bits)]
+    bits = np.append(bits,[0,0,0,0])
     return bits
--- PythonImplementation/SOQPSK-TG_modulation.py
+++ PythonImplementation/SOQPSK-TG_modulation.py
@@ -3,13 +3,50 @@ import numpy as np
 import math, cmath
 import matplotlib.pyplot as plt
 
+preamble = [0,0,1,1,0,1,1,0,0,0,0,0,0,0];
+
+def SOQPSK_TG_Demod(preamble, signal, SPS):
+    samples_per_bit = SPS/2
+
+    phase = (np.unwrap(np.angle(signal)) - 0.785416) / np.pi
+
+    filtered = np.zeros(len(phase))
+    for x in range(1, len(phase)-1, 1):
+        filtered[x] = phase[x+1] - phase[x]
+
+    filter_offset = 0.015
+    padded = np.zeros(len(phase))
+    for i in range(0, len(padded), 1):
+        if(filtered[i] > filter_offset):
+            padded[i] = 1
+        elif(filtered[i] < -1 * filter_offset):
+            padded[i] = -1
+
+    alpha = np.zeros(int(len(padded)/samples_per_bit))
+    for x in range(1, len(alpha) - 1, 1):
+        alpha[x-1] = padded[(int((x-1) * samples_per_bit))]
+
+    starter = [preamble[len(preamble) - 2], preamble[len(preamble) - 1]]
+    bits = np.zeros(len(alpha))
+    bits[0] = starter[0]
+    bits[1] = starter[1]
+    for x in range(2, len(alpha) - 1, 1):
+        if((([1 if x%2 == 0 else -1][0]) * (2 * bits[x - 1] - 1) * (1- bits[x-2])) == alpha[x]):
+            bits[x] = 1
+
+    bits = bits[4:len(bits)]
+    bits = np.append(bits,[0,0,0,0])
+    return bits
+
 # Parameters specific to SOQPSK-TG
 T,  E, SPS = 1, 1, 20
 
 # Random bitstream generation
-number_of_bits = 1000
-u = [0 if random() < 0.5 else 1 for _ in range(number_of_bits)]
-
+number_of_bits = 200
+u = preamble + [0 if random() < 0.5 else 1 for _ in range(number_of_bits)]
+#print(u)
+#u = [0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1,1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1]
+number_of_bits = len(u)
 # (Standard) Ternary Precorder
 alpha = [0,0] + [((-1)**(k))*(2*u[k-1] - 1)*(u[k] - u[k-2]) for k in range(2, number_of_bits)]
 
@@ -66,7 +103,12 @@ def eye_diagram(signal, bits_per_symbol, n):
     plt.xticks([0, math.floor(n/2), n], ['-0.5','0','0.5'])
 
     plt.show()
-
+    bits = SOQPSK_TG_Demod(preamble, signal, 20)
+    bitlen = range(len(bits))
+    ulen = range(len(u))
+    plt.scatter(bitlen, bits, edgecolors='blue')
+    plt.scatter(ulen, u, marker='*')
+    plt.show()
 # Eye diagram
 eye_diagram(signal, 10, 40)
 
